6c04f3d98382c77d9d0041b0812827d699febbfd
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-16 14:55:28 +0530

    change the DB name

--- config/DB.js
+++ config/DB.js
@@ -6,8 +6,8 @@ import { DB_Name } from "../constant.js";
 const connectDB = async ()=>{
 
     try {
-        const connect= await mongoose.connect(`${process.env.MONGODB_URL}/DB_Name`)
-        console.log(`Mongo DB Connect Successfully!! with the URL -${connect.connection.host}`)
+        const connect= await mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`)
+        console.log(`Mongo DB Connect Successfully!! with the URL -${connect.connection.host} ${connect.connection.name}`)
     } catch (error) {
         console.log("error in the mongoDB connection",error);
         process.exit(1);
