3054196fcc6993456ee7ebe49a27b7a70112102d
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-05-17 16:41:36 +0530

    create the OptionalProtection feature

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import UserRoute from "./routes/User.routes.js"
 import TestRoute from "./routes/Test.routes.js";
 import AuthRoute from "./routes/Auth.routes.js"
 import LeaderBoardRoute from "./routes/Leaderboard.routes.js"
+import OptionalProtect from './middleware/Auth.middleware.js';
 
 
 
@@ -12,7 +13,7 @@ import LeaderBoardRoute from "./routes/Leaderboard.routes.js"
 const app = express();
 app.use(express.json())
 app.use(cors())
-
+app.use(OptionalProtect)
 app.use("/api/user",UserRoute);
 app.use("/api/test",TestRoute);
 app.use("/api/Auth",AuthRoute);
