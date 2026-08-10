cedea02be0a42b929a94e6077a1b8a6966356521
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-08-11 01:05:06 +0530

    change the server starting method and rapup in the starting function

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ import connectDB from './config/DB.js';
 import { connectRedis } from './config/redis.js';
 import app from "./src/app.js"
 
- await connectRedis();
+ const startServer = async () => {
+  try {
+    await connectDB();
+    await connectRedis();
 
-connectDB().then(()=>{
+    app.listen(process.env.PORT, () => {
+      console.log('Server started at Port :', process.env.PORT);
+    });
 
-    
-    app.listen(process.env.PORT,()=>{console.log("Server started at Port :",process.env.PORT)});
-})
-.catch( (error)=>{
-    console.log(error);
-    
-})
+  } catch (error) {
+    console.error('Server startup failed:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
