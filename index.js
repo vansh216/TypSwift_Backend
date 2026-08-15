e131f31c1c676fe71fc821dec810c7d1a7b2bd6d
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-08-15 23:56:38 +0530

    Feat:Change the server add the socket connection usign http and connect to the server to socket server

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import 'dotenv/config'
 
-
+import http from 'http';
 import connectDB from './config/DB.js';
 import { connectRedis } from './config/redis.js';
 import app from "./src/app.js"
+import initializeSocket from './socket/index.js';
 
  const startServer = async () => {
   try {
     await connectDB();
     await connectRedis();
 
-    app.listen(process.env.PORT, () => {
+    const server = http.createServer(app);
+    initializeSocket(server);
+
+    server.listen(process.env.PORT, () => {
       console.log('Server started at Port :', process.env.PORT);
     });
 
